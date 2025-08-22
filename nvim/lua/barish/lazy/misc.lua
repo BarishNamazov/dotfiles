98c72d6b77540163f0dede58348ff1ab76ffc356
Abutalib (Barish) Namazov <barish2003@gmail.com>
2025-08-22 14:03:04 +0400

    Add gitignore and lazygit

--- nvim/lua/barish/lazy/misc.lua
+++ nvim/lua/barish/lazy/misc.lua
@@ -47,4 +47,22 @@ return {
     {
         "github/copilot.vim"
     },
+
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
+    }
 }
