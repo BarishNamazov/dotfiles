7a4dbb52820fbf84483b01d643897df176f64f50
Abutalib (Barish) Namazov <barish2003@gmail.com>
2025-10-03 22:05:41 +0400

    remove plugin

--- nvim/lua/barish/lazy/cmp.lua
+++ nvim/lua/barish/lazy/cmp.lua
@@ -32,12 +32,6 @@ return {
             }),
         })
 
-        cmp.setup.cmdline({ "/", "?" }, {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = "buffer" },
-            },
-        })
 
         cmp.setup.cmdline(":", {
             mapping = cmp.mapping.preset.cmdline(),
